f5de1f222334635ca110c412fcf717d7532dd169
Arrain <140903846+Arrainna@users.noreply.github.com>
2024-04-09 16:14:34 +0700

    user_activity

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\activityLog;
 use Illuminate\Http\Request;
 use App\Models\Workspace;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class MainController extends Controller
 {
-    public function index()
-    {
+    public function index() {
         $data['workspaces'] = Workspace::get()->all();
         return view('home', $data);
     }
-
-
+    public function activityLogInLogOut() {
+        $data['workspaces'] = Workspace::get()->all();
+        $activityLog['user_activity'] = activityLog::get()->all();
+        return view('activity_log', $data,$activityLog);
+    }
 }
 
 
--- routes/web.php
+++ routes/web.php
@@ -6,45 +6,41 @@ use App\Http\Controllers\MainController;
 use App\Http\Controllers\WorkspaceController;
 
 
-Route::group(['middleware' => 'guest'], function () {
+Route::group(['middleware' => 'guest'],function(){
 
-    Route::get('/register', [AuthController::class, 'signup'])->name('signup');
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::get('/register',[AuthController::class,'signup'])->name('signup');
+    Route::post('/register',[AuthController::class,'register'])->name('register');
 
-    Route::get('/login', [AuthController::class, 'signin'])->name('signin');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::get('/login',[AuthController::class,'signin'])->name('signin');
+    Route::post('/login',[AuthController::class,'login'])->name('login');
 
-    Route::get('/forgot-password', [AuthController::class, 'forgot'])->name('forgot');
+    Route::get('/forgot-password',[AuthController::class,'forgot'])->name('forgot');
 });
 
 
 
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => 'auth'],function(){
     Route::get('/', [MainController::class, 'index'])->name('home.index');
-    // Route::get('/home', [MainController::class, 'index'])->name('home.index');
+    Route::get('/home', [MainController::class, 'index'])->name('home.index');
 
     Route::get('/workspace/create', [WorkspaceController::class, 'create'])->name('workspace.create');
     Route::post('/workspace/create', [WorkspaceController::class, 'store'])->name('workspace.store');
 
     Route::get('/workspace/{workspace}', [WorkspaceController::class, 'index'])->name('workspace.index');
-    Route::get('/workspace/{workspace}/collection', [WorkspaceController::class, 'collections'])->name('workspace.collections');
-    Route::get('/workspace/{workspace}/collection/edit_{collection}', [WorkspaceController::class, 'editCollection'])->name('workspace.editCollection');
+    Route::get('/workspace/{workspace}/collections', [WorkspaceController::class, 'collections'])->name('workspace.collections');
     Route::get('/workspace/{workspace}/history', [WorkspaceController::class, 'history'])->name('workspace.history');
     Route::get('/workspace/{workspace}/trash', [WorkspaceController::class, 'trash'])->name('workspace.trash');
+    Route::get('/add-collections-tabs/{collection}', [WorkspaceController::class, 'addToCollectionTabs'])->name('add.collection.tabs');
     Route::get('/delete-collections-tabs/{collection}', [WorkspaceController::class, 'deleteFromCollectionTabs'])->name('delete.collection.tabs');
-    Route::post('/workspace/{workspace}/collection/importfile/{collection}',[WorkspaceController::class,'import_file'])->name('importFile');
+    Route::get('/workspace/{workspace}/collections/{collection}', [WorkspaceController::class, 'viewCollection'])->name('workspace.viewCollection');
 
-    Route::get('/workspace/{workspace}/setting', [WorkspaceController::class, 'setting'])->name('workspace.setting');
     Route::get('/workspace/{workspace}/delete-collection', [WorkspaceController::class, 'deleteCollection'])->name('workspace.deleteCollection');
 
-    Route::get('/delete-collection/{collection}', [WorkspaceController::class, 'delete_collection'])->name('delete.collection');
-    Route::get('/move-to-trash-collection/{collection}', [WorkspaceController::class, 'moveToTrash'])->name('move.trash.collection');
-    Route::get('/recover-collection/{collection}', [WorkspaceController::class, 'recovery_trash'])->name('recovery.trash');
-    Route::get('/delete-workspace/{workspace}', [WorkspaceController::class, 'delete_workspace'])->name('workspace.deleteWorkspace');
 
-    Route::get('/add-new-tab', [WorkspaceController::class, 'addNewTabs'])->name('add.new.tabs');
 
-    Route::post('/export', [WorkspaceController::class, 'wordExport'])->name('home.exportfile');
+    Route::get('/workspace/{workspace}/add-file', [WorkspaceController::class, 'add_file'])->name('workspace.add_file');
 
-    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::delete('/logout',[AuthController::class,'logout'])->name('logout');
+    Route::get('users/{id}/active/login/logout',[MainController::class, 'activityLogInLogOut'])->name('active/login/logout');
+
 });
